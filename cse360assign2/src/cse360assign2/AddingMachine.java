712d4a3a582730da377d1ecd6eddf505f3987982
Molly Luther <meluther@asu.edu>
2019-10-09 22:49:15 -0700

    Second version with all methods implemented.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -3,71 +3,79 @@
  * Assignment 2
  * The AddingMachine.java file contains the class definition for an adding 
  * machine object. There exists a private instance variable, a constructor, a 
- * get, add, substract, and toString method, as well as a clear method to reset
+ * get, add, subtract, and toString method, as well as a clear method to reset
  * the adding machine. 
  */
 
-/** AddingMachine contains one private int instance variable that represents the
- * total of all values added and subtracted, one constructor, a get method to 
- * return the total, adding and subtracting functions to adjust the total, a 
- * toString() function to print transaction history, and a clear function
- * to reset the adding machine. 
- *
- * @author mollyluther
- *
- */
 package cse360assign2;
 
+/** 
+ * AddingMachine contains one private int instance variable that represents the
+ * total of all values added and subtracted, one private String instance 
+ * variable that represents the transaction history, one constructor, a get 
+ * method to return the total, adding and subtracting functions to modify the 
+ * total, a toString() function to print the history, and a clear function to 
+ * reset the adding machine's history back to 0. 
+ *
+ * @author mollyluther
+ */
 public class AddingMachine {
 	
 	private int total;
+	private String history = ""; // make empty otherwise prints null in toString
 	
-	/** Initializes AddingMachine object with int value of 0. There is no return
+	/** 
+	 * Initializes AddingMachine object with int value of 0. There is no return
 	 * value because this is a constructor. 
 	 */
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history += total;
 	}
 	
-	/** Returns current total value.    
+	/** 
+	 * Returns current total value.    
 	 * 
 	 * @return 	total	int representing total calculated by adding machine
-	 * 
 	 */
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
-	/** Adds parameter to current total and stores sum in total. 
+	/** 
+	 * Adds parameter to current total and stores sum in total. 
 	 * 
 	 * @param 	value	int to add to total 
-	 * 
 	 */
 	public void add (int value) {
-		
+		total += value;
+		history += " + "; 
+		history += value; 
 	}
 	
-	/** Subtracts parameter from current total and stores difference in total. 
+	/** 
+	 * Subtracts parameter from current total and stores difference in total. 
 	 * 
 	 * @param 	value	int to subtract from total 
-	 * 
 	 */
 	public void subtract (int value) {
-		
+		total -= value;  
+		history += " - "; 
+		history += value;
 	}
 	
 	/** Returns String containing adding machine's transaction history.    
 	 * 
-	 * @return 	string representation of adding machine
-	 * 
+	 * @return 	String 	representation of adding machine past operations
 	 */
 	public String toString () {
-		return "";
+		return history;
 	}
 
-	/** Resets adding machine,    
+	/** 
+	 * Resets adding machine back to 0.  
 	 */
-	public void clear() {
-	
+	public void clear () {
+		history = "0"; 
 	}
 }
